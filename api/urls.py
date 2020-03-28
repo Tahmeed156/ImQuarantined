68eff90d4f5016ef0c5e4afc4ebf5f2992ed2061
Tahmeed156 <tahmeedtarek@gmail.com>
2020-03-29 01:18:19 +0600

    Intial views, urls for API

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,18 @@
-from django.urls import path
-
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers
 from . import views
 
+router = routers.DefaultRouter()
+router.register('players', views.PlayersViewset)
+
 urlpatterns = [
+
     path('', views.index, name='index'),
+
+    # login
+    path('login', views.PlayerLogin.asView(), name='player-login'),
+
+    path('models/', include(router.urls))
 ]
+
--- api/views.py
+++ api/views.py
@@ -1,6 +1,37 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from .serializers import PlayerSerializer
+from .models import Player
 
 
 def index(request):
     return HttpResponse("Hello, plebs. Welcome to my API!")
+
+
+class PlayerLogin(APIView):
+
+    # authentication_classes = [TokenAuthentication]
+    # permission_classes = [IsAuthenticated]
+
+    @staticmethod
+    def post(request):
+        # end_users = EndUser.objects.select_related('address').filter(user_type='repairer').all()
+        # serializer = EndUserSerializer(end_users,
+        # fields=[constants.Default.COL_ID, constants.EndUser.COL_ADDR],
+        # many=True)
+        # return Response(serializer.data)
+        pass
+
+
+# Testing database models
+
+class PlayerViewSet(viewsets.ModelViewSet):
+
+    # authentication_classes = [TokenAuthentication]
+    # permission_classes = [IsAuthenticated]
+
+    queryset = Player.objects.all()
+    serializer_class = PlayerSerializer
