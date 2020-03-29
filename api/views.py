f63d2bf031c85d1d2f214f6c9bbbb7c9c9d4bed0
Tahmeed156 <tahmeedtarek@gmail.com>
2020-03-29 20:25:11 +0600

    Update locations - API request & response
    - Saving to db left
    - Inner bool logic left

--- api/views.py
+++ api/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.http import HttpResponse
 from rest_framework import viewsets
 from rest_framework.views import APIView
@@ -5,7 +7,7 @@ from rest_framework.response import Response
 
 from imquarantined.settings import BASE_DIR
 from .serializers import PlayerSerializer
-from .models import Player
+from .models import Player, Location
 
 import firebase_admin
 from firebase_admin import auth
@@ -63,37 +65,34 @@ class PlayerLogin(APIView):
 
 
 class UpdateLocation(APIView):
-
+    
     # authentication_classes = [TokenAuthentication]
     # permission_classes = [IsAuthenticated]
 
     @staticmethod
     def post(request):
-
-        id_token = request.POST['id_token']
-
-        # Setting up firebase_app
-        cred = credentials.Certificate("/media/tmdfx/Stuff/Code/imquarantined/imquarantined-firebase.json")
-        firebase = firebase_admin.initialize_app(cred, name='firebase')
-
-        # Verifying an id_token
-        decoded_token = auth.verify_id_token(id_token, firebase)
-        uid = decoded_token['uid']
-        user = auth.get_user(uid, firebase)
-
         response = {
             "success": True,
-            'message': 'Working on it!',
-            'data': {
-                'id_token': id_token,
-                'uid': user.uid,
-                'photo': user.photo_url,
-                'phone': user.phone_number,
-                'name': user.display_name,
-                'email': user.email
-            }
+            'message': 'Still at home',
+            'data': {}
         }
+
+        locations_string = request.POST['locations']
+        locations = json.loads(locations_string)
+        for count, loc in enumerate(locations):
+            lat = loc['lat']
+            long = loc['long']
+            alti = loc['alti']
+            new_loc = Location(
+                latitude=loc['lat'],
+                longitude=loc['long'],
+                altitude=loc['alti']
+            )
+            # new_loc.save()
+            response['data'][count] = loc
+
         return Response(response)
+
 
 # Testing database models
 
